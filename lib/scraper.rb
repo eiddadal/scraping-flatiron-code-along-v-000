22a7049b4b0a62898248e458edc08a61a29ace83
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-26 21:29:57 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -27,6 +27,8 @@ class Scraper
     Course.all.each do |course|
       if course.title
         puts "Title: #{course.title}"
+        puts "Schedule: #{course.schedule}"
+        puts "Description: #{course.description}"
       end 
     end 
   end 
