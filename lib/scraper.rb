62f44c66e442ab4434d539037180824c5bf00e95
Dalya Eidda <dalia.eidda@gmail.com>
2019-05-26 21:24:25 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -11,13 +11,7 @@ class Scraper
   
   def get_courses
     self.get_page.css(".post")
-    doc.css(".post").each do |post|
-      course = Course.new
-      course.title = post.css("h2").text
-      course.schedule = post.css(".date").text
-      course.description = post.css("p").text
-    end 
-    binding.pry
+    
   end 
   
 end 
